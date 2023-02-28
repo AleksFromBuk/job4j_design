e3cbac7e225dc0dd8ee17ecd6c32dc64b04cd6a1
Alexey Layko <Layko1982@mail.ru>
2023-02-28 08:37:44 +0300

    5.1.1. Итератор для двухмерного массива int[][] upgrade...

--- src/main/java/ru/job4j/iterator/MatrixIt.java
+++ src/main/java/ru/job4j/iterator/MatrixIt.java
@@ -14,11 +14,11 @@ public class MatrixIt implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        while (row < data.length && (data[row].length == 0 || data[row].length == column)) {
+        while (row < data.length &&  data[row].length == column) {
             column = 0;
             row++;
         }
-        return row < data.length && column < data[row].length;
+        return row < data.length;
     }
 
     @Override
