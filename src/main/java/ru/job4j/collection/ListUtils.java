c4fcb3d03950475fedd8f6089fd58cbb602bfbee
Alexey Layko <Layko1982@mail.ru>
2023-03-13 18:13:05 +0300

    7. ListIterator

---
+++ src/main/java/ru/job4j/collection/ListUtils.java
@@ -0,0 +1,56 @@
+package ru.job4j.collection;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ListUtils {
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index + 1, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index + 1) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        List<T> res = list.stream()
+                .filter(filter)
+                .collect(Collectors.toList());
+        list.retainAll(res);
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.set(value);
+            }
+        }
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        List<T> res = list.stream()
+                .filter(o -> !elements.contains(o))
+                .collect(Collectors.toList());
+        list.retainAll(res);
+    }
+}
---
+++ src/test/java/ru/job4j/collection/ListUtilsTest.java
@@ -0,0 +1,65 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ListUtilsTest {
+
+    private List<Integer> input;
+
+    @BeforeEach
+    void setUp() {
+        input = new ArrayList<>(Arrays.asList(1, 3));
+    }
+
+    @Test
+    void whenAddBefore() {
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenAddBeforeWithInvalidIndex() {
+        assertThatThrownBy(() -> ListUtils.addBefore(input, 3, 2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenAddAfter() {
+        ListUtils.addAfter(input, 0, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenRemoveIf() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        assertThat(input).hasSize(10).containsSequence(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> expected = Arrays.asList(0, 2, 4, 6, 8);
+        ListUtils.removeIf(input, a -> a % 2 == 0);
+        assertThat(input).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    void whenReplaceIf() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        assertThat(input).hasSize(10).containsSequence(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> expected = Arrays.asList(0, -1, 2, -1, 4, -1, 6, -1, 8, -1);
+        ListUtils.replaceIf(input, a -> a % 2 != 0, -1);
+        assertThat(input).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    void whenRemoveAll() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, -1, 2, -3, 4, -5, 6, -7, 8, -9));
+        assertThat(input).hasSize(10).containsSequence(0, -1, 2, -3, 4, -5, 6, -7, 8, -9);
+        List<Integer> expected = Arrays.asList(-1, -3, -5, -7, -9);
+        ListUtils.removeAll(input, Arrays.asList(0, 2, 4, 6, 8));
+        assertThat(input).containsExactlyElementsOf(expected);
+    }
+}
