6824984c52904e50394470f96864bfd49190cf4f
Alexey Layko <Layko1982@mail.ru>
2023-05-01 22:36:49 +0300

    2.0. FileOutputStream [#252488]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -9,7 +9,7 @@ public class ResultFile {
             out.write(System.lineSeparator().getBytes());
             for (int i = 1; i <= 10; i++) {
                 for (int j = 1; j <= 10; j++) {
-                    out.write((i*j + " ").getBytes());
+                    out.write((i * j + " ").getBytes());
                 }
                 out.write(System.lineSeparator().getBytes());
             }
