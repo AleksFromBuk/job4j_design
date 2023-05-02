0b2cf0708afba0e358048eada7180b988f91da11
Alexey Layko <Layko1982@mail.ru>
2023-05-02 11:53:46 +0300

    3. Классы вывода PrintStream / PrintWriter [#504999 #413632]

---
+++ src/main/java/ru/job4j/io/PrintUsage.java
@@ -0,0 +1,16 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class PrintUsage {
+    public static void main(String[] args) {
+        try (PrintStream stream = new PrintStream(new FileOutputStream("data/print.txt"));
+        PrintWriter writer = new PrintWriter("data/write.txt")) {
+            stream.println("Из PrintStream в FileOutputStream");
+            stream.write("Новая строка".getBytes());
+            writer.println("Новое сообщение в новом файле");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
