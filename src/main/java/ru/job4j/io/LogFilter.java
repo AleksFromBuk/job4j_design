70a41b4de85cc7ba87105dbac99b8569e7aca576
Alexey Layko <Layko1982@mail.ru>
2023-05-03 11:07:50 +0300

    5.1. BufferedReader [#252489 #413750] top method

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogFilter {
-    public List<String> filter(String file) {
+    public List<String> filterOld(String file) {
         List<String> data = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             StringBuilder str = new StringBuilder();
@@ -15,11 +15,26 @@ public class LogFilter {
                     .map(o -> o.split(" "))
                     .filter(arr -> "404".equals(arr[arr.length - 2]))
                     .map(arr -> (Arrays.stream(arr)
-                                .reduce(str,
-                                        (a, b) -> new StringBuilder(a).append(b).append(" "),
-                                        StringBuilder::append)).toString())
+                            .reduce(str,
+                                    (a, b) -> new StringBuilder(a).append(b).append(" "),
+                                    StringBuilder::append)).toString())
                     .collect(Collectors.toList());
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public List<String> filter(String file) {
+        List<String> data = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                String[] arr = line.split(" ");
+                if ("404".equals(arr[arr.length - 2])) {
+                    data.add(line);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
