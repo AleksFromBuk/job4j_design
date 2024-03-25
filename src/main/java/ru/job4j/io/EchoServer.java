65a058a9245e12cf3ff61a2ae31a8d5fbdbeabda
Alexey Layko <Layko1982@mail.ru>
2024-03-25 15:54:13 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,8 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,10 +11,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
     private final static String HELLO = "Hello";
     private final static String EXIT = "Exit";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -31,6 +35,8 @@ public class EchoServer {
                     output.flush();
                 }
             }
+        } catch (IOException e) {
+            LOG.error("Ошибка ввода-вывода", e);
         }
     }
 
