0c1e0bdd24b54b3fbddef411a75b5eb02239cedf
Alexey Layko <Layko1982@mail.ru>
2024-03-25 15:41:06 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -13,5 +13,23 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        byte a = -127;
+        short b = -1;
+        int c = 7;
+        long d = 3L;
+        float e = -0.14F;
+        double f = 3.14D;
+        boolean bool = true;
+        char symbol = '♥';
+        LOG.debug("""
+                information about types of local variables:\s
+                byte: {},\s
+                short: {},\s
+                int: {},\s
+                long: {},\s
+                float: {},\s
+                double: {},\s
+                boolean: {},\s
+                char: {}\s""", a, b, c, d, e, f, bool, symbol);
     }
 }
